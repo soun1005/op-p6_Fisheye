d818f2f1d2e40715d366c415f640f104d6a3d0a8
Soeun Lee <thdms1005@gmail.com>
2022-09-20 18:49:07 +0200

    dom and link

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,24 +1,54 @@
-function photographerFactory(data) {
-  const { name, portrait } = data;
+// factory method to create objects.
+function photographerFactory(data) { // eslint-disable-line no-unused-vars
+  // let name = data.name;
+  // let portrait = data.portrait;
+  // 객체구조분해
+  const {
+    name, portrait, id, city, country, tagline, price,
+  } = data;
 
-  // photos of photographer
+  // photos of photographers
+  // data.portrait = address of the imgs
   const picture = `assets/photographers/${portrait}`;
 
-  // list of dom for usercard
+  // to create 'article' in HTML.
   function getUserCardDOM() {
+    // eslint-disable-next-line spaced-comment
+    /**** create tags in HTML(=createElement).
+     <article> & <a> ************************/
     const article = document.createElement('article');
-
+    const photographerLink = document.createElement('a');
+    photographerLink.href = `photographer.html?id=${id}`;
+    // <img>
     const img = document.createElement('img');
-
     img.setAttribute('src', picture);
-
+    img.setAttribute('alt', `photo of ${name}`);
+    // <h2>
     const h2 = document.createElement('h2');
-
     h2.textContent = name;
 
-    article.appendChild(img);
+    // articleInfo > hometown(h3), tagline(p), price(p)
+    const articleInfo = document.createElement('article');
+    const tagLineElement = document.createElement('p');
+    const homeTown = document.createElement('h3');
+    const priceElement = document.createElement('p');
+    homeTown.textContent = `${city}, ${country}`;
+    tagLineElement.textContent = tagline;
+    priceElement.textContent = `${price}€ / jour`;
 
-    article.appendChild(h2);
+    // class names added in HTML
+    articleInfo.classList.add('info');
+    homeTown.classList.add('city');
+    tagLineElement.classList.add('tagLine');
+    priceElement.classList.add('price');
+
+    // appending elements in a parent elements
+    photographerLink.appendChild(img);
+    photographerLink.appendChild(h2);
+    article.appendChild(articleInfo);
+    articleInfo.appendChild(homeTown);
+    articleInfo.appendChild(tagLineElement);
+    articleInfo.appendChild(priceElement);
 
     return (article);
   }
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,36 @@
-async function getPhotographers() {
-  let fetchedData = {};
+/* global photographerFactory */
+/* eslint no-undef: "error" */
 
+// eslint-disable-next-line spaced-comment
+/**************** fetch data from Json file ****************/
+async function getDataJson() {
+  let fetchedData = { photographers: [], media: [] };
   await fetch('data/photographers.json')
     .then(async (response) => {
       fetchedData = await response.json();
-    }).catch(() => {
-      fetchedData = { photographers: [], media: [] };
     });
-  console.log(fetchedData);
   return fetchedData;
 }
 
+// displayData = display data in the landing page
 async function displayData(photographers) {
+  // select div called 'photographer section'
   const photographersSection = document.querySelector('.photographer_section');
 
+  // each photographers are going to have userCardDom inside <div class="photographer_section">
   photographers.forEach((photographer) => {
+    // photographerModel = factory method + parameters(data)
     const photographerModel = photographerFactory(photographer);
+    // userCardDOM = user html tags
     const userCardDOM = photographerModel.getUserCardDOM();
+    // inside <section> put user html tags
     photographersSection.appendChild(userCardDOM);
   });
 }
 
 async function init() {
   // Récupère les datas des photographes
-  const { photographers } = await getPhotographers();
+  const { photographers } = await getDataJson();
   displayData(photographers);
 }
 
